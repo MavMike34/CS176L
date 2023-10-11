d405585794db6f6cb4fc37554bcacd8fd48ce79d
Mike Montulet <49600950+MavMike34@users.noreply.github.com>
2023-10-11 19:32:26 -0400

    Week 6 Problems

---
+++ Week6Problem1.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Week6Problem1 {
+
+	public static void main(String[] args) {
+		
+		Scanner scnr = new Scanner(System.in);
+		
+		boolean infiniteLoop = true;
+		
+		while (infiniteLoop == true) {
+			System.out.println("Enter a name: ");
+			String userName = scnr.next();
+			System.out.println(userName);
+		}
+		
+
+	}
+
+}
